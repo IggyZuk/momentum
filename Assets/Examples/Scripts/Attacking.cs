96f504be6e772ff1462f4f8eaf80df1ff5549494
Iggy Zuk <IgnatusZuk@gmail.com>
2018-09-22 23:14:09 +0100

    Some visuals for attacking example.

--- Assets/Examples/Scripts/Attacking.cs
+++ Assets/Examples/Scripts/Attacking.cs
@@ -11,24 +11,44 @@ namespace Momentum
 
         public Text text;
 
+        public Transform target;
+
+        public Transform rest;
+        public Transform prepare;
+        public Transform swing;
+
         void Start()
         {
             text.text = "Ready";
+
+            Lerp(target, rest, 1f);
 
             Task attackTask = new Task(this);
 
             attackTask
                 .Name("Preparing...")
                 .Time(prepareTime)
-                .OnUpdate(data => text.text = "Preparing... : " + data.Progress.ToString("0.0"))
+                .OnUpdate(data =>
+                {
+                    text.text = "Preparing... : " + data.Progress.ToString("0.0");
+                    Lerp(rest, prepare, Ease.InOutSine(data.Progress));
+                })
                 .Next(this)
                 .Name("Swing!!!")
                 .Time(swingTime)
-                .OnUpdate(data => text.text = "Swing! : " + data.Progress.ToString("0.0"))
+                .OnUpdate(data =>
+                {
+                    text.text = "Swing! : " + data.Progress.ToString("0.0");
+                    Lerp(prepare, swing, Ease.InOutSine(data.Progress));
+                })
                 .Next(this)
                 .Name("Cooldown...")
                 .Time(cooldownTime)
-                .OnUpdate(data => text.text = "Cooldown... : " + data.Progress.ToString("0.0"))
+                .OnUpdate(data =>
+                {
+                    text.text = "Cooldown... : " + data.Progress.ToString("0.0");
+                    Lerp(swing, rest, Ease.InOutSine(data.Progress));
+                })
                 .OnComplete(_ => text.text = "Ready");
 
             Task.Run(this)
@@ -41,6 +61,12 @@ namespace Momentum
                         if (!attackTask.IsActiveWithChildren()) attackTask.Start();
                     }
                 });
+        }
+
+        void Lerp(Transform a, Transform b, float t)
+        {
+            target.position = Vector3.Lerp(a.position, b.position, t);
+            target.rotation = Quaternion.Slerp(a.rotation, b.rotation, t);
         }
 
     }
